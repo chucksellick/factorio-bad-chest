e716a1f8658d92732501565f630249431d78b51d
petehurst <petehurst@users.noreply.github.com>
2016-04-25 23:47:32 +0100

    Improve gulp build

--- gulpfile.js
+++ gulpfile.js
@@ -4,29 +4,37 @@ var path = require("path");
 
 var gulp = require('gulp');
 
-var srcPaths = ["src/**/*.lua", "src/**/*.png", "src/**/*.json", "src/**/*.cfg"];
+var infoPath = "./src/info.json";
+var srcPaths = ["src/**/*.lua", "src/**/*.png", "src/**/*.cfg"];
+
+var homePath = process.env[(process.platform === 'win32') ? 'USERPROFILE' : 'HOME'];
+var appPath = (process.platform === 'win32') ? "AppData\\Roaming\\Factorio" : "Library/Application Support/factorio";
+
+var factorioPath = path.join(homePath, appPath);
+
+if (!fs.existsSync(factorioPath)) {
+  throw new Error("Factorio not found at: " + factorioPath + "\nPlease configure correct Factorio base path in config.js");
+}
+
+var modPath = path.join(factorioPath, "mods");
+var buildPath = null;
+
+gulp.task('devPath', function() {
+  var infoJson = JSON.parse(fs.readFileSync(infoPath));
+  buildPath = path.join(modPath, infoJson.name + "_" + infoJson.version);
+  // TODO: Remove old folder if path changed
+  return gulp.src(infoPath)
+    .pipe(gulp.dest(buildPath));
+});
 
 gulp.task('devBuild', function() {
-
-  var infoJson = require("./src/info.json");
-  var homePath = process.env[(process.platform === 'win32') ? 'USERPROFILE' : 'HOME'];
-  var appPath = (process.platform === 'win32') ? "AppData\\Roaming\\Factorio" : "Library/Application Support/factorio";
-
-  var factorioPath = path.join(homePath, appPath);
-
-  if (!fs.existsSync(factorioPath)) {
-    throw new Error("Factorio not found at: " + factorioPath + "\nPlease configure correct Factorio base path in config.js");
-  }
-
-  var modPath = path.join(factorioPath, "mods");
-  var buildPath = path.join(modPath, infoJson.name + "_" + infoJson.version);
-
   return gulp.src(srcPaths)
     .pipe(gulp.dest(buildPath));
 });
 
 gulp.task("devWatch", function() {
+  gulp.watch(infoPath, ["devPath"]);
   gulp.watch(srcPaths, ["devBuild"]);
-})
+});
 
-gulp.task('default', ["devBuild", "devWatch"]);
+gulp.task('default', ["devPath", "devBuild", "devWatch"]);
